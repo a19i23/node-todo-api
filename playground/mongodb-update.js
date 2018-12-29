581d85d637b0446ed6c94cfaeffd6e492e0aa79a
Alan Villa <alanvilla919@gmail.com>
2018-12-28 17:57:02 -0800

    adding update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,46 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c267fc400853776e68a94bf')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c1ffe180803d133dee2ebc4')
+    }, {
+        $set: {
+            name: 'Alam'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close(true);
+
+
+
+});
