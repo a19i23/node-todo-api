e657c2dd98ab67c1cbb6695f5eae4898127327a5
Alan Villa <alanvilla919@gmail.com>
2019-01-03 20:16:28 -0800

    added tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -16,7 +16,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -155,4 +157,52 @@ describe('DELETE /todos/:id', () => {
             .expect(404)
             .end(done);
     });
-})
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        //grab id of first item
+        var hexID = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+
+        //update text, set completed true
+        request(app)
+            .patch(`/todos/${hexID}`)
+            .send({
+                text,
+                completed: true
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end(done);
+        // 200
+        // text is changed, completed is true, completedAt is a number .toBeA
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        //grab id of second todo item
+        var hexID = todos[1]._id.toHexString();
+        var text = 'This should be the new text too';
+
+        request(app)
+            .patch(`/todos/${hexID}`)
+            .send({
+                text,
+                completed: false
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist;
+            })
+            .end(done);
+        //update text, set completed to false
+        //200
+        // text is changed, completed false, completedAt is null .toNotExist
+    });
+});
